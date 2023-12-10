7844c6d0d01ce4e2f941e9906fa1e81bb71a453d
demasarvin11 <demasarvin11@gmail.com>
2023-12-10 15:34:20 +0700

    Fix navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,75 @@
-import { Link } from "react-router-dom"
-
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { HiOutlineMenuAlt3, HiOutlineX } from "react-icons/hi";
 const Navbar = () => {
-    return (
-        <div className="py-6 bg-lime-700">
-        <div className="container mx-auto flex gap-5 items-center">
-          <Link to="/" className="text-white font-serif text-3xl pr-6 font-semibold">Muslim App</Link>
-          <Link to="/quran" className="text-white text-lg font-semibold hover:text-lime-300">Quran</Link>
-          <Link to="/doa" className="text-white text-lg font-semibold hover:text-lime-300">Doa</Link>
-          <Link to="/dzikir" className="text-white text-lg font-semibold hover:text-lime-300">Dzikir</Link>
-          <Link to="/hadist" className="text-white text-lg font-semibold hover:text-lime-300">Hadist</Link>
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navLinkMenu = [
+    {
+      name: "Quran",
+      path: "/quran",
+    },
+    {
+      name: "Doa",
+      path: "/doa",
+    },
+    {
+      name: "Dzikir",
+      path: "/dzikir",
+    },
+    {
+      name: "Hadist",
+      path: "/hadist",
+    },
+  ];
+  return (
+    <nav className="sticky top-0 z-50 w-full bg-lime-700">
+      <div className="container mx-auto gap-14 px-6 md:flex md:items-center md:px-8">
+        <div>
+          <div className="flex justify-between py-4 md:block ">
+            <NavLink
+              to="/"
+              className="font-serif text-3xl font-semibold text-white"
+            >
+              Muslim App
+            </NavLink>
+            <div className="md:hidden">
+              <button
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+                className="block text-white"
+              >
+                {isMenuOpen ? (
+                  <HiOutlineX className={`text-3xl hover:text-lime-300`} />
+                ) : (
+                  <HiOutlineMenuAlt3 className={`text-3xl hover:text-lime-300`} />
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div
+            className={`flex-1 justify-self-center pb-4 md:block md:pb-0 ${
+              isMenuOpen ? "block" : "hidden"
+            }`}
+          >
+            <div className="space-y-4 text-lg font-semibold text-white md:flex md:space-x-5 md:space-y-0">
+              {navLinkMenu.map((item, index) => (
+                <div key={index}>
+                  <NavLink
+                    to={item.path}
+                    className={({ isActive }) =>
+                      isActive ? "active text-lime-300" : "hover:text-lime-300"
+                    }
+                  >
+                    {item.name}
+                  </NavLink>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
-    )
-}
-export default Navbar
+    </nav>
+  );
+};
+export default Navbar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import HomePage from './pages/Home.jsx'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import HomePage from "./pages/Home.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <HomePage />,
   },
-])
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
